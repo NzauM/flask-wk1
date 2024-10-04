f2d9cfc2081054623b89961f0616dda2ae38c7af
NzauM <mercywaenu16@gmail.com>
2024-10-04 13:21:19 +0300

    serialize relationships and add a post api

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, make_response,redirect, abort, request
-from models import db,Coffee
+from flask import Flask, make_response,redirect, abort, request,jsonify
+from models import db,Coffee,Order
 from flask_migrate import Migrate
 
 app = Flask(__name__)
@@ -40,7 +40,35 @@ def list_coffees():
     resp_body = coffee_list
     resp_code = 200
     return make_response(resp_body,resp_code)
-        
+
+@app.route('/orders')
+def list_orders():
+    # get all orders
+    # orders_list = []
+    all_orders = Order.query.all()
+    # for order in all_orders:
+    #     order_dict = order.to_dict()
+    #     orders_list.append(order_dict)
+    # return make_response((orders_list),200)
+    order_list = [order.to_dict() for order in all_orders]
+    return make_response(order_list,200)
+
+@app.route('/addcoffee',methods=['POST'])
+def addCoffee():
+    # receive data from request
+    # use this data to create an instance
+    # add this instance to session and commit it
+    coffee_data = request.json
+    print(coffee_data) #{'name': 'Black Coffee', 'price': 50.0}
+    new_coffee = Coffee(name=coffee_data['name'], price=coffee_data['price'])
+    db.session.add(new_coffee)
+    db.session.commit()
+    return make_response({'success':'New Coffee created'},201)
+
+
+
+
+
 
 
 
--- models.py
+++ models.py
@@ -18,7 +18,7 @@ class Coffee(db.Model, SerializerMixin):
     customers = association_proxy('orders','customer', creator=lambda coffeeObj: Order(coffee = coffeeObj))
 
 
-class Customer(db.Model):
+class Customer(db.Model,SerializerMixin):
     # A customer has many orders
     # A cofee can have many customers by making many orders
     # So Customer has Many Coffees through Orders
@@ -36,12 +36,15 @@ class Customer(db.Model):
     # coffees = [order.coffee for order in allOrders where order.customer = thiscustomer]
 
 
-class Order(db.Model):
+class Order(db.Model,SerializerMixin):
 
     # An order belongs to a customer
     # An order belongs to a coffee
 
     __tablename__ = 'orders'
+    serialize_rules = ("-customer.orders",'-coffee.orders')
+    # serialize_only=('customer_id','coffee')
+    
 
     id = db.Column(db.Integer, primary_key=True)
     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
@@ -50,3 +53,28 @@ class Order(db.Model):
     customer = db.relationship('Customer',back_populates='orders')
     coffee = db.relationship('Coffee',back_populates = 'orders')
 
+# /orders/1
+# The first order
+# {
+#     "id":1,
+#     "customer_id":1,
+#     "coffee_id":1,
+#     "customer":{
+#         "id":1,
+#         "name":"Mercy",
+#         "phone_no":789,
+#         "orders":[{
+#             "id":1,
+#             "customer_id":1,
+#             "coffee_id":1
+#             "customer":{
+#                 "id"
+#                 "name"
+#                 "phone_no"
+#                 "coffees":[]
+#             }
+
+#         }]
+#     }
+
+# }
